b07bfb8b2620f607f86b9ae06136183b0a67e0b0
AdhyaksaWP <adhy.wp@gmail.com>
2021-02-18 22:53:15 +0700

    Revert "Nomer 1"
    This reverts commit 199a6c8dfbd7316e11b9e5bd4ca2242963e4b814.

--- Nomer 5.py
+++
@@ -1,8 +0,0 @@
-import itertools, random
-
-deck = list(itertools.product(range(1,14),['kriting','wajik','hati','sekop',]))
-
-random.shuffle(deck)
-print('kamu mendapat : ')
-for i in range(5):
-    print(deck[i][0],' dari', deck[i][1])
